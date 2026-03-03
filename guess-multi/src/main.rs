f8b4ef39e7693716e513ea10846a2d7d80fe4523
M Developer Rust Log <m@localraspberrypi>
2026-03-03 17:26:42 +0000

    Replaced guess/src/main.rs with new version as I expected the order to be flawd.

--- guess-multi/src/main.rs
+++ guess-multi/src/main.rs
@@ -1,38 +1,28 @@
+use std::cmp::Ordering;
 use std::io;
 use rand::Rng;
-use std::cmp::Ordering;
 
 fn main() {
-
-   
-   println!("Input a guress number ");
-   let mut guess = String::new();
-   
-   io::stdin()
-      .read_line(&mut guess).expect("There is a read error");
-   println!("The guess is {guess}");
-
-   let guess: u32 = guess.trim().parse() {
-       ok(num) => num,
-       Err(_) => {
-
-        println!("Please type a number");
-        return;           
-
-       }
-   }; 
-
-   println!("You guessed {guess}");
-
+   println!("Guess the number!");
    let secret_number = rand::thread_rng().gen_range(1..=100);
-   println!("The random number is {secret_number}");
-
-   match guess.cmp(&secret_number){
-      Ordering::Less => println!("This is smaller"),
-      Ordering::Greater => println!("This is larger"),
-      Ordering::Equal => {
-         println!("This is correct and matching");
-         break;
-      }   
-   }    
+   loop {
+          println!("Please input your guess.");
+          let mut guess = String::new();
+io::stdin()
+.read_line(&mut guess)
+.expect("Failed to read line");
+let guess: u32 = match guess.trim().parse() {
+Ok(num) => num,
+Err(_) => continue,
+};
+println!("You guessed: {guess}");
+match guess.cmp(&secret_number) {
+Ordering::Less => println!("Too small!"),
+Ordering::Greater => println!("Too big!"),
+Ordering::Equal => {
+println!("You win!");
+break;
+}
+}
+}
 }
