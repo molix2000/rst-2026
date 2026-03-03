51cd05277a30085bd478f473d24d267c9047f418
M Developer Rust Log <m@localraspberrypi>
2026-03-03 22:44:22 +0000

    Added some refernces to the ML components of the reasearch project, this would come as more advanced at later stages. IT was good to keep refernce to it.

---
+++ ML-Components.ml
@@ -0,0 +1,8 @@
+#Introduction</p>
+
+It is of paramount importance for one to consider the very important role that Rust can play.</p>
+There are many components that are being re-written in Rust, partially if not fully.</p>
+We can look into the famous changes in Python components such as the package manager.</p>
+
+Below is a good article on how to design a component in Rust:</p>
+https://tiberiusferreira.github.io/blog/posts/designing_autograd_system_rust_first_steps/
