0b059092adac1ac4e85200d83eebe03c458bfba7
M Developer Rust Log <m@localraspberrypi>
2026-03-03 17:21:16 +0000

    Changes to the structure on std::cmp:Ordering. beggining ans lines 31-33, file guess-multi/src/main.rs

---
+++ guess-multi/target/debug/deps/ordering-2722ed69cdbf18a6.d
@@ -0,0 +1,5 @@
+/home/devops/Documents/source-code/rst-2026/guess-multi/target/debug/deps/ordering-2722ed69cdbf18a6.d: src/main.rs
+
+/home/devops/Documents/source-code/rst-2026/guess-multi/target/debug/deps/libordering-2722ed69cdbf18a6.rmeta: src/main.rs
+
+src/main.rs:
