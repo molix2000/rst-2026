c3209bef3fd7728279f72ee37a23c7ba31963e59
M Developer Rust Log <m@localraspberrypi>
2026-03-03 23:07:40 +0000

    More ML links added

--- ML-Components.ml
+++ ML-Components.ml
@@ -6,3 +6,4 @@ We can look into the famous changes in Python components such as the package man
 
 Below is a good article on how to design a component in Rust:</p>
 https://tiberiusferreira.github.io/blog/posts/designing_autograd_system_rust_first_steps/
+https://rufflewind.com/2016-12-30/reverse-mode-automatic-differentiation
