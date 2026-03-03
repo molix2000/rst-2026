78b58e8ac38b7f8a55a96ab8e1e8629c2fb9fb3b
M Developer Rust Log <m@localraspberrypi>
2026-03-03 15:46:08 +0000

    Created optional statements to cater for multiple inputs.

--- guess-multi/src/main.rs
+++ guess-multi/src/main.rs
@@ -12,7 +12,17 @@ fn main() {
       .read_line(&mut guess).expect("There is a read error");
    println!("The guess is {guess}");
 
-   let guess: u32 = guess.trim().parse().expect("Please type number");
+   let guess: u32 = guess.trim().parse() {
+       ok(num) => num,
+       Err(_) => {
+
+        println!("Please type a number");
+        return;           
+
+       }
+   }; 
+
+   println!("You guessed {guess}");
 
    let secret_number = rand::thread_rng().gen_range(1..=100);
    println!("The random number is {secret_number}");
