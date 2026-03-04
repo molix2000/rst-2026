e42e2e0d9aa6726d344bf4cbb311454ac7623c8a
M Developer Rust Log <m@localraspberrypi>
2026-03-04 12:56:27 +0000

    Checked  in connected updates.

--- test-2-5/src/main.rs
+++ test-2-5/src/main.rs
@@ -4,5 +4,8 @@ fn main() {
     println!("Apples are {apples}");
     let mut apples = 55;
     println!("The mutable version of apples as shadowed are {apples}");
-
+    let x = 5;
+    println!("The value of direct x is {x}");
+    let x = 6;
+    println!("The re-assigned value of x is {x}");
 }
