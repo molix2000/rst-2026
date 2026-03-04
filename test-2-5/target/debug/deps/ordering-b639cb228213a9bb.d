296479bdb83cca60ae7fd887d2e952dbce8e193c
M Developer Rust Log <m@localraspberrypi>
2026-03-04 13:35:20 +0000

    set changes

---
+++ test-2-5/target/debug/deps/ordering-b639cb228213a9bb.d
@@ -0,0 +1,5 @@
+/home/devops/Documents/source-code/rst-2026/test-2-5/target/debug/deps/ordering-b639cb228213a9bb.d: src/main.rs
+
+/home/devops/Documents/source-code/rst-2026/test-2-5/target/debug/deps/libordering-b639cb228213a9bb.rmeta: src/main.rs
+
+src/main.rs:
