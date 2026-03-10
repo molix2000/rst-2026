c1ff89073d25c0eaddff6a94b8846e47b62b9c03
M Developer Rust Log <m@localraspberrypi>
2026-03-10 16:13:12 +0000

    testing string representation line 4. changes and effects.

---
+++ test-3-8/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("String manipulation in Rust!");
+    let a = [1,2,3,4,5];
+    println!("Array: {:?}", a);
+    println!("Array : {:?}",a);
+
+    
+}
---
+++ test-3-8/target/debug/deps/test_3_8-542d11389f844257.d
@@ -0,0 +1,5 @@
+/home/devops/Documents/source-code/rst-2026/test-3-8/target/debug/deps/test_3_8-542d11389f844257.d: src/main.rs
+
+/home/devops/Documents/source-code/rst-2026/test-3-8/target/debug/deps/test_3_8-542d11389f844257: src/main.rs
+
+src/main.rs:
