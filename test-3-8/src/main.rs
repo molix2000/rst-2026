b4fc9a3e0c6fd3418838af326ced794fa0c73069
M Developer Rust Log <m@localraspberrypi>
2026-03-10 16:48:06 +0000

    testing the array parsing.

--- test-3-8/src/main.rs
+++ test-3-8/src/main.rs
@@ -4,11 +4,11 @@ fn main() {
     println!("String manipulation in Rust!");
     let a = [1,2,3,4,5];
     println!("Array: {:?}", a);
-    
+    println!("Please enter an array index");
     let mut index = String::new();
     io::stdin().red_line(&mut index).expect(This is wrong type of input");
     
-    let index: usize = index/trim().parse.expect("This is the wrong type of input");
+    let index: usize = index.trim().parse.expect("This is the wrong type of input");
 
     let element = a[index];
     println!(" The value of element at index {index} , element {element}", index,element);
