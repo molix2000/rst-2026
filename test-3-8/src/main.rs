baa76f85e1415d6462554d8f0935a53f1d23d397
M Developer Rust Log <m@localraspberrypi>
2026-03-10 16:34:53 +0000

    test

--- test-3-8/src/main.rs
+++ test-3-8/src/main.rs
@@ -1,8 +1,16 @@
+use std::io;
+
 fn main() {
     println!("String manipulation in Rust!");
     let a = [1,2,3,4,5];
     println!("Array: {:?}", a);
-    println!("Array : {:?}",a);
+    
+    let mut index = String::new();
+    io::stdin().red_line(&mut index).expect(This is wrong type of input");
+    
+    let index: usize = index/trim().parse.expect("This is the wrong type of input");
 
+    let element = a[index];
+    println!(" The value of element at index {index} , element {element}", index,element);
     
 }
