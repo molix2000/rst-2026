b4660275685b01e00e6a98e428cd7e89c04e989c
M Developer Rust Log <m@localraspberrypi>
2026-03-10 19:28:34 +0000

    testing string position alternation

--- test-3-8/src/main4.rs
+++ test-3-8/src/main4.rs
@@ -3,8 +3,9 @@ fn main(){
 
    let mut x1 = String::from("Triumph400X");
    println!("the value of the product is {x1}");
+   const POSIT : u8 = 11;
    let r1 = &mut x1;
-   r1.insert(x1.len(-1), 's');
+   r1.insert(POSIT.into(), 's');
    println!("This is r1 after insert {r1}");
 
 }
