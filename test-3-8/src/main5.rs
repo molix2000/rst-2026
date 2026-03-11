acef6a4a9984d2896d7b8d0c435aafc86413aebb
M Developer Rust Log <m@localraspberrypi>
2026-03-11 13:07:44 +0000

    testing Rust changes on chapter 3.

---
+++ test-3-8/src/main5.rs
@@ -0,0 +1,21 @@
+use std::io;
+fn main () {
+
+   println!("String manupulation");
+   let mut a = [1,2,3,4,5];
+   let mut index = String::new();
+   println!("Please give an index");
+   io::stdin().read_line(&mut index).expect("This index is not in the correct type");
+   let index: usize = index.trim().parse().expect("This index in not in the tisht format");
+   if index >= a.len() {
+      println!("Out of range");
+      return;
+   }
+   let element = a[index];
+   println!("element is {element}, for index {index}");
+
+
+
+
+
+}
---
+++ test-3-8/target/debug/deps/test_3_8-c6cd2764f756237a.d
@@ -0,0 +1,5 @@
+/home/devops/Documents/source-code/rst-2026/test-3-8/target/debug/deps/test_3_8-c6cd2764f756237a.d: src/main.rs
+
+/home/devops/Documents/source-code/rst-2026/test-3-8/target/debug/deps/libtest_3_8-c6cd2764f756237a.rmeta: src/main.rs
+
+src/main.rs:
