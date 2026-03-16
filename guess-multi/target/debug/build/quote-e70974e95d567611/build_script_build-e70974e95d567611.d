d4deb45b7ecae4b83f2801fbb49ae84ae11ab42a
Mo R <MoR@localhost>
2026-03-16 13:02:47 +0000

    Added new Rust config files from new environment

---
+++ guess-multi/target/debug/build/quote-e70974e95d567611/build_script_build-e70974e95d567611.d
@@ -0,0 +1,5 @@
+/home/devops/Downloads/rst-2026/guess-multi/target/debug/build/quote-e70974e95d567611/build_script_build-e70974e95d567611.d: /home/devops/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.44/build.rs
+
+/home/devops/Downloads/rst-2026/guess-multi/target/debug/build/quote-e70974e95d567611/build_script_build-e70974e95d567611: /home/devops/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.44/build.rs
+
+/home/devops/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.44/build.rs:
