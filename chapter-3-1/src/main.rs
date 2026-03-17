f5cf212be53ac8c024e97bcd16c47acb6c590abc
M Developer Rust Log <m@localraspberrypi>
2026-03-17 18:44:12 +0000

    more mathematical funcitons

--- chapter-3-1/src/main.rs
+++ chapter-3-1/src/main.rs
@@ -21,5 +21,7 @@ fn main() {
     let t = true;
     let f: bool = false;
     println!("{t} and {f}");
-    
+    let tup: (i32, f64, u8) = (500,6.4,1);
+    let (x,y,z) = tup;
+    println!("{x}, {y}, {z}");
 }
