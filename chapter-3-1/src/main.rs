1f017ce76524682fde48142eacdf8a064c0fa91d
M Developer Rust Log <m@localraspberrypi>
2026-03-17 18:32:37 +0000

    test

--- chapter-3-1/src/main.rs
+++ chapter-3-1/src/main.rs
@@ -8,4 +8,18 @@ fn main() {
     println!("The space_one length is {space_one}");
     let guess : u32 = "42".parse().expect("there as an error");
     println!("Guess is {guess}");
+    let sum = 5 + 10;
+    println!("the sum is {sum}");
+    let diff = 9.45 - 4.39;
+    println!("The diff is {diff}");
+    let product = 4 * 30;
+    println!("product is {product}");
+    let quoitent = 56.7 / 32.2;
+    println!("the quotitent is {quoitent}");
+    let remainder = 43 % 5;
+    println!("The remainder is {remainder}");
+    let t = true;
+    let f: bool = false;
+    println!("{t} and {f}");
+    
 }
