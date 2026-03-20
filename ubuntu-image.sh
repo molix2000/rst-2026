251bd219df1dd96e5adbd7f5c3de99dfc7c5a47c
Molix Rawdi <molixrawdi@planetearth.universe>
2026-03-20 14:08:30 +0000

    Changed Docker script, removed carrage return from it.

--- ubuntu-image.sh
+++ ubuntu-image.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
-docker run -it --rm \n  -v . \n  --workdir / \n  ubuntu/rust:1.75-24.04_edge exec 
+docker run -it --rm   -v .   --workdir / ubuntu/rust:1.75-24.04_edge exec 
 ##\n  cargo build --release
