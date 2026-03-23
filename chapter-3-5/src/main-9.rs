c2dcf14ca9de978e57284a8a931f4f98eeadb91c
M Developer Rust Log <m@localraspberrypi>
2026-03-23 17:32:13 +0000

    testing scalar types

---
+++ chapter-3-5/src/main-9.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let c = 'z';
+    let z: char = 'Z';
+    let heart_eyed_cat = 'D';
+
+    println!("The c char is {c}");
+    println!("The z char is {z}");
+    println!("The heart eye is {heart_eyed_cat}");
+}
