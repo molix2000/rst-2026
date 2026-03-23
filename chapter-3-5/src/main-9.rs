7efb7dcb5609e30cfc88e8758f1b35c2a37e50b0
M Developer Rust Log <m@localraspberrypi>
2026-03-23 18:36:14 +0000

    setup multiple trim examples for testing

--- chapter-3-5/src/main-9.rs
+++ chapter-3-5/src/main-9.rs
@@ -6,8 +6,33 @@ fn main() {
     println!("The c char is {c}");
     println!("The z char is {z}");
     println!("The heart eye is {heart_eyed_cat}");
-    let mut a = (i32,f64, u8) = (500,6.4, 8);
+    let mut a: (i32,f64,u8) = (500,6.4,8);
+    let (x,y,z) = a;
     let five_oo = a.0;
     let six_4 = a.1;
     let eight = a.2;
+    main2();
+}
+
+fn main2() {
+  // create some strings
+  let string1 = " Welcome to Edpresso    ";
+  let string2 = "Educative is the best!     ";
+  let string3 = "     Rust is very interesting!";
+
+  // trim the strings
+  let trim1 = string1.trim();
+  let trim2 = string2.trim();
+  let trim3 = string3.trim();
+
+  // print the trims
+  println!("The string before trim is '{}' and length is {}", string1, string1.len());
+  println!("The string when trimmed is '{}' and length is {}", trim1, trim1.len());
+
+  println!("\nThe string before trim is '{}' and length is {}", string2, string2.len());
+  println!("The string when trimmed is '{}' and length is {}", trim2, trim2.len());
+
+  println!("\nThe string before trim is '{}' and length is {}", string3, string3.len());
+  println!("The string when trimmed is '{}' and length is {}", trim3, trim3.len());
+
 }
