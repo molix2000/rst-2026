5fc0fde3ffd4deac767803bf2fff1d48aea6f431
M Developer Rust Log <m@localraspberrypi>
2026-03-23 18:14:56 +0000

    Removed extra binary files

--- chapter-3-5/src/main-9.rs
+++ chapter-3-5/src/main-9.rs
@@ -6,4 +6,8 @@ fn main() {
     println!("The c char is {c}");
     println!("The z char is {z}");
     println!("The heart eye is {heart_eyed_cat}");
+    let mut a = (i32,f64, u8) = (500,6.4, 8);
+    let five_oo = a.0;
+    let six_4 = a.1;
+    let eight = a.2;
 }
