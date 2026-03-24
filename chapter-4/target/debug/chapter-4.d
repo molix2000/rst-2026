24331e88a85ff48129e6c1d88ff0364e924d8da1
M Developer Rust Log <m@localraspberrypi>
2026-03-24 16:14:04 +0000

    Chapter 4 components

---
+++ chapter-4/target/debug/chapter-4.d
@@ -0,0 +1,1 @@
+/home/devops/Documents/source-code/rst-2026/chapter-4/target/debug/chapter-4: /home/devops/Documents/source-code/rst-2026/chapter-4/src/main.rs
---
+++ chapter-4/target/debug/deps/chapter_4-11221f6702296e8f.d
@@ -0,0 +1,5 @@
+/home/devops/Documents/source-code/rst-2026/chapter-4/target/debug/deps/chapter_4-11221f6702296e8f.d: src/main.rs
+
+/home/devops/Documents/source-code/rst-2026/chapter-4/target/debug/deps/chapter_4-11221f6702296e8f: src/main.rs
+
+src/main.rs:
