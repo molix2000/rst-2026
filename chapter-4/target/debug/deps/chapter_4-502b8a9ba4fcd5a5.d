fc7d5bb7b42ab2295989827d7d8839346938b38d
M Developer Rust Log <m@localraspberrypi>
2026-03-24 16:13:53 +0000

    more sring examples from chapter 4

---
+++ chapter-4/target/debug/deps/chapter_4-502b8a9ba4fcd5a5.d
@@ -0,0 +1,5 @@
+/home/devops/Documents/source-code/rst-2026/chapter-4/target/debug/deps/chapter_4-502b8a9ba4fcd5a5.d: src/main.rs
+
+/home/devops/Documents/source-code/rst-2026/chapter-4/target/debug/deps/libchapter_4-502b8a9ba4fcd5a5.rmeta: src/main.rs
+
+src/main.rs:
