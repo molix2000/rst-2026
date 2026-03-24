7b0384b7b9e74dbf755b332232821cc5636f656e
M Developer Rust Log <m@localraspberrypi>
2026-03-24 17:29:51 +0000

    More examples on strings are more than premitives , instead datastrucutres with emmence importance.

--- chapter-4/src/main.rs
+++ chapter-4/src/main.rs
@@ -3,4 +3,17 @@ fn main() {
     let mut s = String::from("This is chapter 4");
     s.push_str(" OF the Rust Book");
     println!("{s}");
+
+    let mut stringer: &str = "There is a light";
+    stringer = "That never Goes out";
+    println!("{stringer}");
+ 
+    let mut stringer2: &str = "There is a light ";
+    let mut stringer3:&str = "That never goes out";
+    //stringer2.push_str(stringer3); Kept as good reference to pitfalls
+    //println!("{stringer2}");
+
+    // One cannot add the two string either, so what to do?
+    let stringer4 = format!("{}{}", stringer2,stringer3);
+    println!("{stringer4}");
 }
