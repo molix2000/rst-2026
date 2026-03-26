de8cad5aa88714dd0988a46038ee4acbbdc83949
M Developer Rust Log <m@localraspberrypi>
2026-03-26 17:58:02 +0000

    More string manipulation examples.

---
+++ chapter-4/str_10.rs
@@ -0,0 +1,35 @@
+fn main(){
+
+    let text = "mazda mx5";
+    if text.contains("mx5") {
+       println!("Found the correct sports car");
+
+    } 
+    if let Some(index) = text.find("mazda") {
+        println!("Mazda starts with index {}",index);
+
+    }
+
+    if text.starts_with("mazda") {
+       println!("This starts with mazda");
+    }
+
+    if text.ends_with("mx5") {
+
+        println!("the text ends with mx5");
+    }
+
+    let car_list = "Mazda,Toyota,Ford,Audi";
+    let car_selection: Vec<&str> = car_list.split(',').collect();
+    println!("{:?}" ,car_selection);
+
+    let bike_list = "Yamaha Suzuki Honda Triumph Ducati Ducati";
+    let bike_brands: Vec<&str> = bike_list.split_whitespace().collect();
+    println!("{:?}", bike_brands);
+
+    let new_bike_brands = bike_list.replace("Yamaha","RoyalEnfield");
+    println!("New Moto brands {}",new_bike_brands);
+
+    let new_brands_list = bike_list.replacen("Ducati", "KTM", 1);
+    println!("New brands list {}: ", new_brands_list);
+}
