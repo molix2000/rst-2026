88a48a62a71b53e5d7f08c6d77cb2fe3e1004b14
M Developer Rust Log <m@localraspberrypi>
2026-03-26 19:20:22 +0000

    More regexp examples

---
+++ chapter4-1/target/debug/chapter-4.d
@@ -0,0 +1,1 @@
+/home/devops/Documents/source-code/rst-2026/chapter4-1/target/debug/chapter-4: /home/devops/Documents/source-code/rst-2026/chapter4-1/src/main.rs
---
+++ chapter4-1/target/debug/deps/chapter_4-6ab394c637e744cd.d
@@ -0,0 +1,5 @@
+/home/devops/Documents/source-code/rst-2026/chapter4-1/target/debug/deps/chapter_4-6ab394c637e744cd.d: src/main.rs
+
+/home/devops/Documents/source-code/rst-2026/chapter4-1/target/debug/deps/libchapter_4-6ab394c637e744cd.rmeta: src/main.rs
+
+src/main.rs:
