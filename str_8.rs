7d3b65a46f013d6736ed433c754a47b18918ecd2
M Developer Rust Log <m@localraspberrypi>
2026-03-26 16:21:48 +0000

    More string examples

---
+++ str_8.rs
@@ -0,0 +1,23 @@
+fn main(){
+
+ let bytes: &[u8] = &[77,33,40,55];
+ let str_frm_bytes = String::from_utf8(bytes.to_vec()).expect("err");
+
+ println!("str_frm_bytes:{str_frm_bytes}");
+
+ let num = 46;
+ println!("{},Num",num);
+ let string_from_num = num.to_string();
+ println!("string_from_num: {string_from_num}");
+
+ let rust: String = String::from("Rust");
+ let road: String = "Road".to_string();
+ let greeting = format!("{} {}", rust, road);
+ println!("{greeting}");
+
+
+
+
+
+}
+
---
+++ str_9.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let title: String = String::from("Rust Volumes");
+    println!("Title:{}", title);
+
+    let greeting = title + "This chapter is 5";
+    println!("{greeting}");
+}
