81082c57c6e85838698afd856201d2ea619cedc6
M Developer Rust Log <m@localraspberrypi>
2026-03-26 16:30:21 +0000

    Used the clone as title was used before.

--- str_9.rs
+++ str_9.rs
@@ -2,6 +2,9 @@ fn main() {
     let title: String = String::from("Rust Volumes");
     println!("Title:{}", title);
 
-    let greeting = title + "This chapter is 5";
+    let greeting = title.clone() + "This chapter is 5";
     println!("{greeting}");
+
+    let full_list = format!("{}, {},", title,greeting);
+    println!("{full_list}");
 }
