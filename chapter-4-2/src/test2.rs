a9562fd67fd34c2c2c62ad5af91c8bbb917801c2
M Developer Rust Log <m@localraspberrypi>
2026-03-27 22:43:46 +0000

    tested passing reference to functions.

--- chapter-4-2/src/test2.rs
+++ chapter-4-2/src/test2.rs
@@ -3,4 +3,17 @@ fn main() {
     let s2 = s1.clone();
     println!("S1 is , {}", s1);
     println!("{s2}");
+
+
+    // test references:
+    //
+    let sv = String::from("Rust volumes");
+    let s_len = calculate_length(&sv);
+
+}
+
+fn calculate_length(s: &String)-> usize {
+   
+     s.len();
+  
 }
