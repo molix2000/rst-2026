8356ea28008a5df3e03e7a4d652a70b30a2753a3
M Developer Rust Log <m@localraspberrypi>
2026-03-28 00:12:30 +0000

    More borrow examples

--- chapter-4-2/src/test2.rs
+++ chapter-4-2/src/test2.rs
@@ -1,15 +1,30 @@
 fn main() {
     let s1 = String::from("Rust is rolling");
-    let s2 = s1.clone();
+    let mut s2 = s1.clone();
     println!("S1 is , {}", s1);
     println!("{s2}");
 
     // test references:
     //
-    let sv = String::from("Rust volumes");
-    let s_len = calculate_length(&sv);
+    let mut sv = String::from("Rust volumes");
+    let sv_len = calculate_length(sv.clone());
+    return_val( sv);
+    change( &mut s2);
+    
 }
 
-fn calculate_length(sv: &String)-> usize {
-    sv.len();
+fn calculate_length(sv: String)-> usize {
+    sv.len()
+}
+
+fn return_val(s: String) -> String {
+   println!("{s}");
+   s
+}
+
+
+fn change(some_string: &mut String) {
+    some_string.push_str(" .. Rust");
+
+
 }
