5543a98330392a5a7983ab493e637a6521b3885b
M Developer Rust Log <m@localraspberrypi>
2026-03-28 21:03:52 +0000

    testing shadowing for variables, inner and outer scope for same variable name. can obtain multiple values.

---
+++ chapter-4-2/src/shadowing.rs
@@ -0,0 +1,13 @@
+fn main(){
+
+  let x = 5;
+  let x = x +1;
+  {
+       let x = x * 2;
+       println!("The value of x is in the inner scope = {x}");
+
+  }
+
+     println!("The value of X {}", x);
+
+}
