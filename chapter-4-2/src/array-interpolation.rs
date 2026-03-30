e45024a6211db5f55eecae8b5cb7b9c16d07dfb3
M Developer Rust Log <m@localraspberrypi>
2026-03-30 22:15:18 +0100

    test examples

--- chapter-4-2/src/array-interpolation.rs
+++ chapter-4-2/src/array-interpolation.rs
@@ -1,7 +1,8 @@
 fn main (){
 
     let tup: (i32, f64, u8) = (500, 6.4, 1);
-
+    let months = ["January", "February", "March","April", "May", "June", "July", "August", "September", "October", "November", "December" ];
     println!("{}:", tup.0);
+    println!("{}", months[0]);
 
 }
