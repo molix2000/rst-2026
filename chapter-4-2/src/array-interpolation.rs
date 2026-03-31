63ce2a53b33f8a2d2a60a9108474d6b27c682e6a
M Developer Rust Log <m@localraspberrypi>
2026-03-31 20:41:39 +0100

    more examples with the array elements.

--- chapter-4-2/src/array-interpolation.rs
+++ chapter-4-2/src/array-interpolation.rs
@@ -1,8 +1,16 @@
 fn main (){
 
     let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let mut index = String::new();
     let months = ["January", "February", "March","April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+    println!("Please input index");
+    let mut index = String::new();
+    io::stdin().read_line(&mut index).expect("There is an input error in data type");
+    let index: usize = index.trim().parse().expect("This is an error in input");
+    let element = a [index];
     println!("{}:", tup.0);
     println!("{}", months[0]);
+    let element = a[index];
+    prinln!("The value of the element at index {index} is : {element}");
 
 }
