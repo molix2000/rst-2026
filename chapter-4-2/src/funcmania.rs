8f9e45b055e4823126f86390e47cdd702b3bd306
M Developer Rust Log <m@localraspberrypi>
2026-04-02 16:06:40 +0100

    Added and corrected funcmania structure

---
+++ chapter-4-2/src/funcmania.rs
@@ -0,0 +1,21 @@
+
+
+
+fn main(){
+
+    println!("Funcmania begins");
+    another_func_one();
+    another_func_two(4);
+ } // end of main function
+   //
+
+fn another_func_one() {
+   println!("Other_func_one has landed here");
+}
+
+fn another_func_two(x: i32){
+   println!("The value x is: {x}");
+}
+
+
+
