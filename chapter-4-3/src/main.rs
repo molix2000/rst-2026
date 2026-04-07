98cc2efd515c387f59e4d8b6b739f83ab9829fca
M Developer Rust Log <m@localraspberrypi>
2026-04-07 23:44:23 +0100

    More array examples and connected structure.

--- chapter-4-3/src/main.rs
+++ chapter-4-3/src/main.rs
@@ -1,11 +1,16 @@
+mod stringequal;
+mod tempconvert;
 mod looper;
 mod loopmania;
 use loopmania::loopmania;
 use looper::looper;
+use tempconvert::tempconvert;
+use stringequal::*;
 // use loopmania::*;
 
 fn main() {
     println!("loopmania start");
     loopmania();
     looper();
+    tempconvert();
 }
---
+++ chapter-4-3/src/tempconvert.rs
@@ -0,0 +1,13 @@
+pub fn tempconvert() {
+    // Formulas:
+    // Convert Fahrenheit to Celsius: ºC = (ºF - 32)(0.5556)
+    // Convert Celsius to Fahrenheit: ºF = (ºC * 1.8) + 32
+    let mut feh: i64 = 22;
+    let mut cel: i64 = (feh - 32);
+
+    let mut celtofeh: f64  = 40.0;
+    let fehfromcel: f64 = (celtofeh as f64 * 1.8);
+    //let mut fehfromcel: f64 = celtofeh * 1.8;
+    println!("CEL temp for Feh valued at {feh} is {cel}");
+    println!("FEH temp for Cel valued at {celtofeh} is {fehfromcel}");
+}
