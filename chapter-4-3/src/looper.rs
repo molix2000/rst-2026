080ae2eebf9ca629908ef1e696e4d1bbfe668301
M Developer Rust Log <m@localraspberrypi>
2026-04-07 13:19:18 +0100

    More examples added to now distributed  structure.

---
+++ chapter-4-3/src/looper.rs
@@ -0,0 +1,6 @@
+pub fn looper() {
+    for numb in (100..106).rev() {
+        println!("The numb range is {numb}");
+    }
+    println!("Ignite");
+}
--- chapter-4-3/src/main.rs
+++ chapter-4-3/src/main.rs
@@ -1,8 +1,11 @@
+mod looper;
 mod loopmania;
 use loopmania::loopmania;
+use looper::looper;
 // use loopmania::*;
 
 fn main() {
     println!("loopmania start");
     loopmania();
+    looper();
 }
