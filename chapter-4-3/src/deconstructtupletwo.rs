e26c0c7c62fbbc4f0b17091121a0df87c0b3abe1
M Developer Rust Log <m@localraspberrypi>
2026-04-09 12:50:13 +0100

    more examples of decoupling

---
+++ chapter-4-3/src/deconstructtupletwo.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let s1 = String::from("hello");
+    // We pass a reference (&s1) instead of the whole string
+    let len = calculate_length(&s1); 
+    
+    println!("The length of '{s1}' is {len}.");
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
