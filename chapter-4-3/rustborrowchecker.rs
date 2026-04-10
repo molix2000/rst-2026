0fb7545420f03ff4b774079d9cf0cd4b845bdd17
M Developer Rust Log <m@localraspberrypi>
2026-04-10 10:57:21 +0100

    Added more helpful comments to the examples.

--- chapter-4-3/rustborrowchecker.rs
+++ chapter-4-3/rustborrowchecker.rs
@@ -3,7 +3,9 @@ fn main() {
     let mut x = 1;
     let mx1 = &mut x;
     //let mx2 = &mut x;
-
+    // can't have a number of mutable references to the same object at the same time
     *mx1 = 2;
+    // but can have multiple immutable references to the same object at the same time
+    let mx2 = &x;
     println!("X = {}", mx1);
 }
