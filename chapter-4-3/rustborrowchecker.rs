81119fca30b2389b111e7f623f290e4a8d997a56
M Developer Rust Log <m@localraspberrypi>
2026-04-10 11:08:02 +0100

    More details added to the code, this would help highlight the different pointer types and examples.

--- chapter-4-3/rustborrowchecker.rs
+++ chapter-4-3/rustborrowchecker.rs
@@ -4,9 +4,13 @@ fn main() {
     let mx1 = &mut x;
     //let mx2 = &mut x;
     // can't have a number of mutable references to the same object at the same time
-    *mx1 = 2;
+    
     // but can have multiple immutable references to the same object at the same time
     // reason is cache coherence and race conditions.
+    *mx1 = 2;
+    // The line above is a reference to a location in memory, and the line below is a reference to the value at that location in memory.
+    // Rust has pointers like *mut T *const T and they are regarded as unsafe. 
+    // It also has smart pointers such  as Box<T> Rc<T> and Arc<T> , RefCell<T> and Mutex<T> which are safe to use.
     let mx2 = &x;
     println!("X = {}", mx1);
 }
