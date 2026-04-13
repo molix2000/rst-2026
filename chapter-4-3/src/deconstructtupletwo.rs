a45435bd991d9050d05c794de6302a2f24fb111d
M Developer Rust Log <m@localraspberrypi>
2026-04-13 16:03:34 +0100

    More examples added

--- chapter-4-3/src/deconstructtupletwo.rs
+++ chapter-4-3/src/deconstructtupletwo.rs
@@ -1,7 +1,7 @@
 pub fn deconstructtupletwo() {
     let s1 = String::from("Rust deconstruct");
     // We pass a reference (&s1) instead of the whole string
-    let len = calculate_length(&s1); 
+    let len = calculate_length(s1); 
     
     println!("The length of '{s1}' is {len}.");
 }
