65ccd39e25ac677f20e18c65b1cacf285b2a4cb2
M Developer Rust Log <m@localraspberrypi>
2026-04-21 14:53:44 +0100

    More Changes

---
+++ chapter-4-3/src/experimenter01.rs
@@ -0,0 +1,17 @@
+pub fn main(){
+
+  
+    let vec01 = vec!["Toyota", "Mitsubishi", "Mazda"];
+  
+    display_vec_position_value();
+}
+
+pub fn display_vec_position_value(ve1: vec) -> usize {
+
+       while position < ve1.len(){
+
+           println!("{} ": vex1[position]); 
+       }
+      
+
+}
--- chapter-4-3/target/debug/deps/chapter_4_3-1611f9be3616d292.d
+++ chapter-4-3/target/debug/deps/chapter_4_3-1611f9be3616d292.d
@@ -1,6 +1,6 @@
-/home/devops/Documents/source-code/rst-2026/chapter-4-3/target/debug/deps/chapter_4_3-1611f9be3616d292.d: src/main.rs src/dangle.rs src/looper.rs src/loopmania.rs src/mutabex.rs src/mutatext.rs src/mutcomplex.rs
+/home/devops/Documents/source-code/rst-2026/chapter-4-3/target/debug/deps/chapter_4_3-1611f9be3616d292.d: src/main.rs src/dangle.rs src/looper.rs src/loopmania.rs src/mutabex.rs src/mutatext.rs src/mutcomplex.rs src/usizer.rs
 
-/home/devops/Documents/source-code/rst-2026/chapter-4-3/target/debug/deps/libchapter_4_3-1611f9be3616d292.rmeta: src/main.rs src/dangle.rs src/looper.rs src/loopmania.rs src/mutabex.rs src/mutatext.rs src/mutcomplex.rs
+/home/devops/Documents/source-code/rst-2026/chapter-4-3/target/debug/deps/libchapter_4_3-1611f9be3616d292.rmeta: src/main.rs src/dangle.rs src/looper.rs src/loopmania.rs src/mutabex.rs src/mutatext.rs src/mutcomplex.rs src/usizer.rs
 
 src/main.rs:
 src/dangle.rs:
@@ -9,3 +9,4 @@ src/loopmania.rs:
 src/mutabex.rs:
 src/mutatext.rs:
 src/mutcomplex.rs:
+src/usizer.rs:
