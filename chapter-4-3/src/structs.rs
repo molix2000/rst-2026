5813be226754557bc79e29116ac67b7e9c20d26d
M Developer Rust Log <m@localraspberrypi>
2026-04-29 23:27:04 +0100

    Added struct

--- chapter-4-3/src/structs.rs
+++ chapter-4-3/src/structs.rs
@@ -16,6 +16,14 @@ fn main(){
        sign_in_count: 1,
     };
    user1.email = String::from("2ndemail@localhost.local");
+
+   let user2 = User {
+       active: user1.active,
+       username: user1.username,
+       email: String::from("anoheruser@localhost.local"),
+       sign_in_count: user1.sign_in_count,
+ 
+   };
 }
 
 fn build_user (email: String, username: String) -> {
