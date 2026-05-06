3fda06a925cfc93b117993aa76517a3ac191462f
M Developer Rust Log <m@localraspberrypi>
2026-05-06 10:41:26 +0100

    More change added to the stricture to indicate that if some value are added to the instantiated strusture other incorporated ones are added as defacto.

--- chapter-4-3/fun_struct.rs
+++ chapter-4-3/fun_struct.rs
@@ -12,7 +12,7 @@ pub fn main(){
     let user1_username = String::from("devops");
 
    let user_1_struct = build_user(user1_email,user1_username);
-   
+   println!("The user_1_struct.sign_in_count, {}", user_1_struct.sign_in_count);  
 
 }
 
