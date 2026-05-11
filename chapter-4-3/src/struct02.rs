9b992ddca31cf624d74a1d6f2c291f2d51d3ab83
M Developer Rust Log <m@localraspberrypi>
2026-05-11 11:20:35 +0100

    changes structure test programme

--- chapter-4-3/src/struct02.rs
+++ chapter-4-3/src/struct02.rs
@@ -1,7 +1,7 @@
 struct  Equal_Band {
   name: String,
   address: String,
-}
+  }
 
 pub fn struct02() {
     let eb = Equal_Band {
@@ -12,3 +12,15 @@ pub fn struct02() {
   println!("eb name is {}", eb.name);
   println!("The eb Address is {}", eb.address);
 }
+
+fn main() {
+   let width = 30;
+  let height = 50;
+
+  println!(" The area of the rectangle is {}", area(width , height));
+} 
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
+
