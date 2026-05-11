9f23c09a7cfa2fa26a0fe342390d2e6dea0ddd81
M Developer Rust Log <m@localraspberrypi>
2026-05-11 17:17:02 +0100

    Test structures

--- chapter-4-3/src/struct02.rs
+++ chapter-4-3/src/struct02.rs
@@ -1,21 +1,20 @@
-struct  Equal_Band {
-  name: String,
-  address: String,
-  }
+struct Equal_Band {
+    name: String,
+    address: String,
+}
 
-pub fn struct02() {
+pub fn main() {
     let eb = Equal_Band {
-      name: String::from("Test Subject A"),
-      address: String::from("299 harley cresent, fedora avenue, rockford"),
+        name: String::from("Test Subject A"),
+        address: String::from("299 harley cresent, fedora avenue, rockford"),
     };
 
-  println!("eb name is {}", eb.name);
-  println!("The eb Address is {}", eb.address);
-}
-pub fn main() {
-   let width = 30;
-   let height = 50;
-   println!(" The area of the rectangle is {}", area(width , height));
+    println!("eb name is {}", eb.name);
+    println!("The eb Address is {}", eb.address);
+
+    let width = 30;
+    let height = 50;
+    println!(" The area of the rectangle is {}", area(width, height));
 } 
 
 pub fn area(width: u32, height: u32) -> u32 {
