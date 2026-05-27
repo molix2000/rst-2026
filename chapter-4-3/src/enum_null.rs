b9c448d1935bf9ff99ebf166c32a9c70ba25ba0d
Mo Developer <Mo@localhost>
2026-05-27 11:26:28 +0100

    test changed to enums

--- chapter-4-3/src/enum_null.rs
+++ chapter-4-3/src/enum_null.rs
@@ -9,5 +9,9 @@ fn main() {
    let y: Option<i8> = Some(5);
    let sum = x + y;
    println!("The SUM is : {}", sum);
+   let some_number = Some(5);
+   let some_char = Some('v');
+   let absent_number = Options<i32> = None;
+
 
 }
