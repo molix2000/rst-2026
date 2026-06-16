8afa153f92836a98643500ffeb6743c909856ac1
Molix Rawdi <molixrawdi@planetearth.universe>
2026-06-16 14:54:46 +0100

    I have added more options for the new container as it doesn't have bash, sh instead.

--- Docker-container.sh
+++ Docker-container.sh
@@ -8,3 +8,10 @@ docker run -it --name rust-dev \
   ## To start only:
 
   ## docker start -ai rust-dev
+
+  ## For the newer Rust image I do this as it will not have bash
+
+  docker run -it --name rust-dev \
+  -v ${Rust-path-folder}/rst-2026:/workspace \
+  --entrypoint /bin/sh \
+  ubuntu/rust:1.93-26.04_edge
