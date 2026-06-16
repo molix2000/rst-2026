d59812fe14cdeba28bb076842e1186ecf98975fe
Molix Rawdi <molixrawdi@planetearth.universe>
2026-06-16 12:49:36 +0100

    Added the Docker file and the container constructor script

---
+++ Docker-container.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker run -it --name rust-dev \
+  -v /Users/morawi/Documents/Rust/rst-2026:/workspace \
+  ubuntu/rust:1.93-26.04_edge \
+  bash
---
+++ dockerfile
@@ -0,0 +1,5 @@
+FROM ubuntu/rust:1.93-26.04_edge
+
+WORKDIR /workspace
+
+CMD ["bash"]
