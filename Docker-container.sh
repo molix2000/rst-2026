a27263b22435890ad8904f69bda1114603a7416d
Molix Rawdi <molixrawdi@planetearth.universe>
2026-06-16 14:32:49 +0100

    Added adjustment to the container script

--- Docker-container.sh
+++ Docker-container.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 docker run -it --name rust-dev \
-  -v /Users/morawi/Documents/Rust/rst-2026:/workspace \
-  ubuntu/rust:1.93-26.04_edge \
-  bash
+  -v ${Rust-path-folder}/rst-2026:/workspace \
+  --entrypoint /bin/sh \
+  ubuntu/rust:1.93-26.04_edge
 
+  ## Note: this image does not include bash, so we start /bin/sh instead.
   ## To start only:
 
   ## docker start -ai rust-dev
