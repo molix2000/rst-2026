3bdcc3b1e6fed7e38951401d90ca379249856c69
Molix Rawdi <molixrawdi@planetearth.universe>
2026-06-16 17:16:15 +0100

    more revision testing

--- chapter_4_rev/src/main.rs
+++ chapter_4_rev/src/main.rs
@@ -10,7 +10,23 @@ fn main() {
     // Use the crate/lib to achieve this result.
     io::stdin()
         .read_line(&mut guess_number).expect("Failed to read a reasonable input");
-    println!("You have guessed the number as {}", guess_number);
 
+    println!("You have guessed the number as {}", guess_number);
+    
+    println!(" The beginning of the new chapter, enter guess");
+    
+    let mut guess = String::new();
+    let mut guess2 = String::new();
+
+    io::stdin()
+      .read_line(&mut guess)
+      .expect("Looks like there is an issue with the input format");
+    println!("The guess you enterred is {guess}");
+    
+    println!("Enter Guess2");
+    io::stdin()
+      .read_line(&mut guess2) 
+      .expect("There was an error with the input");
+    println!("Guess one was {guess}, Guess2 was {guess2}");
 
 }
