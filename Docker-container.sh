c17b9670d0b46cc1b9d759a0c3adf71a123c1a6e
Molix Rawdi <molixrawdi@planetearth.universe>
2026-06-16 13:17:02 +0100

    Added a start component to the container script

--- Docker-container.sh
+++ Docker-container.sh
@@ -3,3 +3,7 @@ docker run -it --name rust-dev \
   -v /Users/morawi/Documents/Rust/rst-2026:/workspace \
   ubuntu/rust:1.93-26.04_edge \
   bash
+
+  ## To start only:
+
+  ## docker start -ai rust-dev
