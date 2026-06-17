b573b9f9ca3a07a37d126b27ff28c2ec7b273db8
Molix Rawdi <molixrawdi@planetearth.universe>
2026-06-17 15:41:34 +0100

    More examples tested

--- chapter_4_rev/src/mains.rs
+++ chapter_4_rev/src/mains.rs
@@ -2,11 +2,11 @@ use std::io;
 
 fn main() {
 
-   let mut arla = "Test Data";
+   let mut arla = String::new();
    println!("Input a string");
    io::stdin().read_line(&mut arla).expect("There was an error receiving the input");
-   let index: usize = arla.trim().parse().expect("There is an error with the input from parse inspect");
-   let element = a [arla];
+   let index: usize = arla.trim().parse().expect("There is an error with the i parse inspect");
+   println!("The index is {}",index);
 
 }
 
